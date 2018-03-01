abded8667cca20903f84a32818655ca98c038f8b
Dmitry <gorbunov.webdev@gmail.com>
2018-03-02 00:09:57 +0400

    Changed project's structure

--- popup/src/index.js
+++ popup/src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import {render} from 'react-dom';
-import { Provider } from 'react-redux'
+import React from 'react'
+import { render } from 'react-dom'
 import { createStore } from 'redux'
-import todoApp from './rducers'
+import { Provider } from 'react-redux'
 import App from './components/App'
+import reducer from './reducers'
 
-let store = createStore(todoApp)
+const store = createStore(reducer)
 
 render(
     <Provider store={store}>
