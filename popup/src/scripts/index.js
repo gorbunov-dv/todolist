db01fe910a1295e161c25bfb711e378459dabddf
Dmitry <gorbunov.webdev@gmail.com>
2018-03-01 23:40:44 +0400

    Prepare Entry Point

--- popup/src/scripts/index.js
+++ popup/src/scripts/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import {render} from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import todoApp from './rducers'
+import App from './components/App'
 
-import App from './components/app/App';
+let store = createStore(todoApp)
 
 render(
-    <App/>,
+    <Provider store={store}>
+        <App/>
+    </Provider>,
     document.getElementById('app')
 );
