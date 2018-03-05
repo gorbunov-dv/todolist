ae51b6cd454dd0caa41d6603fd93403cae98168d
Dmitry <gorbunov.webdev@gmail.com>
2018-03-05 23:42:01 +0400

    Changed todos.js

--- popup/src/reducers/todos.js
+++ popup/src/reducers/todos.js
@@ -1,23 +1,39 @@
-const todos = (state = [], action) => {
+const todo = (state, action) => {
     switch (action.type) {
-        case 'ADD_TODO':
-            return [
-                ...state,
-                {
-                    id: action.id,
-                    text: action.text,
-                    completed: false
-                }
-            ]
-        case 'TOGGLE_TODO':
-            return state.map(todo =>
-                (todo.id === action.id)
-                    ? {...todo, completed: !todo.completed}
-                    : todo
-            )
-        default:
-            return state    
+      case 'ADD_TODO':
+        return {
+          id: action.id,
+          text: action.text,
+          completed: false
+        }
+      case 'TOGGLE_TODO':
+        if (state.id !== action.id) {
+          return state
+        }
+  
+        return Object.assign({}, state, {
+          completed: !state.completed
+        })
+  
+      default:
+        return state
     }
-}
-
-export default todos
+  }
+  
+  const todos = (state = [], action) => {
+    switch (action.type) {
+      case 'ADD_TODO':
+        return [
+          ...state,
+          todo(undefined, action)
+        ]
+      case 'TOGGLE_TODO':
+        return state.map(t =>
+          todo(t, action)
+        )
+      default:
+        return state
+    }
+  }
+  
+  export default todos
