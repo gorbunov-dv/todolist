7b15c7a6e39c3ad5d4bdeefdea1d2644e8dbe7d8
Dmitry <gorbunov.webdev@gmail.com>
2018-03-12 23:22:19 +0400

    added the function componentDidMount

--- content/src/scripts/components/app/App.jsx
+++ content/src/scripts/components/app/App.jsx
@@ -3,11 +3,24 @@ import React, {Component} from 'react';
 class App extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      count: 0
+    };
+  }
+
+  componentDidMount() {
+    document.addEventListener(() => {
+      this.setState({
+        count: this.state.count + 1;
+      });
+    })
   }
 
   render() {
     return (
       <div>
+        Hello React {this.state.count}
       </div>
     );
   }
